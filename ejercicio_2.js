d91a860b8b4aa57a6236970c4c8f9c22ff4d4451
Rodevp <rodriguezrodrigor12@gmail.com>
2022-05-05 11:14:23 -0500

    openboot ejer2 mod

--- ejercicio_2.js
+++ ejercicio_2.js
@@ -2,3 +2,15 @@ for (let index = 0; index < 100; index++) {
     if (index % 2 === 0) console.log('par')
         else console.log('impar')
 }
+
+
+let cont = 0
+
+while (cont < 10) {
+
+    if (cont % 2 === 1  || cont === 2) {
+        console.log('Numero primo', cont)
+    }
+
+    cont++
+}
