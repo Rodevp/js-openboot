1c2b24dd3991cbbbac674591ffe8c17f6f71b579
Rodevp <rodriguezrodrigor12@gmail.com>
2022-05-05 10:16:32 -0500

    openboot cap2

---
+++ tipos.js
@@ -0,0 +1,18 @@
+/**
+ * existes 5 tipos de datos primitivos
+ * numbers, strings, Boolean, Null, Undefined
+ * 
+ * objetos -> arrays, objects literals
+ * 
+ */
+
+//valores falsy -> null, undefined, false y 0
+//todos estos valores valdrán como "falso" en determinada condición
+
+console.log("hola")
+console.log(2)
+console.log(undefined)
+console.log(null)
+console.log(false)
+console.log(true)
+
