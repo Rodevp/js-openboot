2d3c7179e44eec97ee789b84ac851ac87206ed9c
Rodevp <rodriguezrodrigor12@gmail.com>
2022-05-09 09:27:39 -0500

    open boot numeros

---
+++ numeros.js
@@ -0,0 +1,12 @@
+let integer = 2
+let decimal = 0.1
+
+//truco para los decimales 
+console.log( Math.round( (0.1 + 0.2) * 100 ) / 100 )
+
+//operaciones y redondeo
+
+let op = 3.2 * 5
+op.toFixed(3) // por parametro recibe la cantidad de decimales que queramos
+//toFixed pasa el numero a string
+
