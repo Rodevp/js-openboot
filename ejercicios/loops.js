713d39fdfb56073ceb12b31977e583543466c58c
Rodevp <rodriguezrodrigor12@gmail.com>
2022-05-10 09:55:28 -0500

    e loops

---
+++ ejercicios/loops.js
@@ -0,0 +1,13 @@
+for(let i = 1; i <= 10; i++) {
+    factorial *= i;
+}
+
+let factorial = 10
+while(factorial){
+
+    if (factorial == 0 || factorial == 1){
+       break;
+    }
+    // ?
+    factorial = factorial - 1;
+}
