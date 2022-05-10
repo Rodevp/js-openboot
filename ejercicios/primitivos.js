20f2eb29af1d0688d9ef5a4f37c97783c3ab0663
Rodevp <rodriguezrodrigor12@gmail.com>
2022-05-10 09:50:43 -0500

    ejer primitivos

---
+++ ejercicios/primitivos.js
@@ -0,0 +1,9 @@
+let name = "rodrigo"
+let age = 21
+let developer = true
+let date = new Date(2000,9,17)
+let favorite_book = {
+    name: 'las aventuras de sherlok holmes',
+    author: 'arthur conan',
+    date: '1859 - 1930'
+}
