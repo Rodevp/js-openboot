b2be5cb3efa4ebba29131a6dc82f6360b8d63518
Rodevp <rodriguezrodrigor12@gmail.com>
2022-05-10 10:00:19 -0500

    e strings

---
+++ ejercicios/strings.js
@@ -0,0 +1,11 @@
+let name_ = 'rodrigo'
+let lastName = 'rodriguez'
+let student = `${name_} - ${lastName}`
+let student_upper = student.toUpperCase()
+let student_lower = student.toLowerCase()
+let lengthCharStudent = student.length
+let primaryLetter = name_[0]
+let lastLetter = name_[-1]
+let zeroSpaces = student.trim()
+let isContain = student.includes(name_)
+
